d0ce798180e30984cc55df82d3a6138ab555a0d9
Rohithv17 <rohithv1396@gmail.com>
2024-02-07 11:28:39 +0530

    Added functionality to insert elements into the linked list

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import com.bridgelabz.SinglyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
+        SinglyLinkedList l = new SinglyLinkedList();
+        l.insertAtFirst(56);  //Adding elements to the linked list(added from left) in the sequence 70->30->56
+        l.insertAtFirst(30);
+        l.insertAtFirst(70);
+
+
+        l.display();
 
     }
 }
--- src/com/bridgelabz/SinglyLinkedList.java
+++ src/com/bridgelabz/SinglyLinkedList.java
@@ -1,4 +1,48 @@
 package com.bridgelabz;
 
 public class SinglyLinkedList {
-}
+
+        private MyNode head;
+        private MyNode tail;
+        private int size;
+        public SinglyLinkedList(){
+            this.size=0;
+        }
+
+        public void insertAtFirst(int value){      //Method to add elements to the list
+            MyNode n= new MyNode(value);
+            n.next=head;
+            head=n;
+            if(tail==null){
+                tail=head;
+            }
+            size=size+1;
+        }
+
+        public void display(){      //To display the elements inserted in the list
+            MyNode temp=head;
+            while(temp!=null){
+                System.out.print(temp.value+"->");
+                temp=temp.next;
+            }
+            System.out.print("End");
+        }
+        private class MyNode{    // A private class for creating a node
+            private int value;
+            private MyNode next;
+
+
+            public MyNode(int value){
+                this.value=value;
+            }
+            public MyNode(int value , MyNode next){
+                this.value=value;
+                this.next=next;
+            }
+
+
+
+        }
+    }
+
+
